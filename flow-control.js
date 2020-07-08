26166ec9e274bf7a073672e8d2e527a98e4c6a89
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-08 11:45:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,12 +29,18 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-     return (age>=13 && age<=19) ? "You are a teenager!":"You are not a teenager!";
+     return (age>=13 && age<=19) ? "You are a teenager!":"You are not a teenager";
 }
 
 function switchAge(age) {
   switch(age){
-    case 13&&14&&15&&16&&17&&18&&19: return "You are a teenager!";
+    case 13: return "You are a teenager!";
+    case 14: return "You are a teenager!";
+    case 15: return "You are a teenager!";
+    case 16: return "You are a teenager!";
+    case 17: return "You are a teenager!";
+    case 18: return "You are a teenager!";
+    case 19: return "You are a teenager!";
     default: return "You have an age";
   }
 }
