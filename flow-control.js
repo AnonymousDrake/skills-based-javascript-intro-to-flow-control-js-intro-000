e2ce29c146dbd83d22f7f0556f7c668bfc2b5902
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-08 11:41:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,7 +34,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age){
-    case 13||14||15||16||17||18||19: return "You are a teenager!"; 
+    case 13||14||15||16||17||18||19: return "You are a teenager!";
     default: return "you have an age";
   }
 }
